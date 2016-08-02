89e00be59e30298b9d81e08006e535a8cbd5ee32
maksyuki <myc20404401@sina.com>
2016-08-02 16:55:57 +0800

    Add some comments
    1.add some comments.

--- System/System_Delay/stm32f10x_system_delay.h
+++ System/System_Delay/stm32f10x_system_delay.h
@@ -1,9 +1,25 @@
-#ifndef __DELAY_H__
-#define __DELAY_H__
+/*************************************************
+Copyright (C), 2016-2016, Team MicroDynamics. 
+
+Filename:    stm32f10x_system_delay.h
+Author:      maksyuki
+Version:     1.0
+Date:        2016.8.2
+Description: declare the time delay function
+Others:      none
+Function List:
+             1. extern void delay_init(void); 
+             2. extern void delay_ms(u16 nms);
+             3. extern void delay_us(u32 nus);
+History:     none
+*************************************************/
+
+#ifndef __STM32F10X_SYSTEM_DELAY_H__
+#define __STM32F10X_SYSTEM_DELAY_H__
 
 #include "stm32f10x.h"
 
-extern void delay_init(void);
-extern void delay_ms(u16 nms);
-extern void delay_us(u32 nus);
+extern void delay_init(void);  /*Initializes the delay function*/
+extern void delay_ms(u16 nms); /*Set time with millisecond precision*/
+extern void delay_us(u32 nus); /*Set time with microsecond precision*/
 #endif

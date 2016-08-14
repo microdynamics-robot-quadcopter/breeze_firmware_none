03b2096c35fc7f146dfe68b42f0b27a77573209f
maksyuki <myc20404401@sina.com>
2016-08-14 22:22:00 +0800

    Modify the battery module comments
    1.modify the battery module comments.

--- System/System_Battery/stm32f10x_system_battery.c
+++ System/System_Battery/stm32f10x_system_battery.c
@@ -1,7 +1,27 @@
+/*******************************************************************************
+Copyright (C), 2016-2016, Team MicroDynamics. 
+
+Filename:    stm32f10x_system_battery.c
+Author:      maksyuki
+Version:     1.0
+Date:        2016.8.14
+Description: implement the battery operation function
+Others:      none
+Function List:
+             1. extern int Get_Temp(void);
+             2. extern int Get_Battery_AD(void);
+             3. extern void Battery_Check(void);
+             4. extern void Battery_Check_Init(void);
+             5. extern u16 Get_ADC(u8 ch);
+             6. extern u16 Get_ADC_Average(u8 ch, u8 times);
+History:     none
+*******************************************************************************/
+
 #include "stm32f10x_system_battery.h"
 #include "stdio.h"
 
 int FLY_ENABLE = 1; //test
+
 //实例化一个电压信息结构体
 Bat_TypedefStructure Battery;
 
@@ -12,32 +32,32 @@ extern int Get_Temp(void)
     u8 i;    
     u16 temp_val = 0;
     float temperate;   
-    for (i = 0; i < 20; i++)                       //读20次,取平均值
+    for (i = 0; i < 20; i++)                       /*读20次，取平均值*/
     {
-        temp_val += Get_ADC(16);                   //温度传感器为通道16
+        temp_val += Get_ADC(16);                   /*温度传感器为通道16*/
     }
     temp_val /= 20;
-    temperate = (float)temp_val * (3.3 / 4096);    //得到温度传感器的电压值
-    temperate = (1.43 - temperate) / 0.0043 + 25;  //计算出当前温度值	 
-    temperate *= 10;                               //扩大十倍,使用小数点后一位
-    return (int)temperate;	 
+    temperate = (float)temp_val * (3.3 / 4096);    /*得到温度传感器的电压值*/
+    temperate = (1.43 - temperate) / 0.0043 + 25;  /*计算出当前温度值*/
+    temperate *= 10;                               /*扩大十倍,使用小数点后一位*/
+    return (int)temperate;
 }
 
-//返回电池电压AD值
+/*返回电池电压AD值*/
 extern int Get_Battery_AD(void)
 {
     return Get_ADC_Average(8, 5);
 }
 
-//检测电池电压
+/*检测电池电压*/
 extern void Battery_Check(void)
 {
-    Battery.BatteryADVal  = Get_Battery_AD();            //电池电压检测  
-    Battery.BatteryRealVal = Battery.BatteryFactor * (Battery.BatteryADVal / 4096.0) * Battery.BatteryADRefVal;  //实际电压 值计算	
-	
+    Battery.BatteryADVal  = Get_Battery_AD();            /*电池电压检测*/
+    Battery.BatteryRealVal = Battery.BatteryFactor * (Battery.BatteryADVal / 4096.0) * Battery.BatteryADRefVal;  /*实际电压 值计算*/
+
     if (FLY_ENABLE)
     {
-        //处于电机开启等飞行状态，在过放电压值（BAT_OVERDIS_VAL）以上0.03v以上，开始报警
+        /*处于电机开启等飞行状态，在过放电压值（BAT_OVERDIS_VAL）以上0.03v以上，开始报警*/
         if (Battery.BatteryRealVal <= (BAT_OVERDIS_VAL + 0.03))
         {
             Battery.BatteryAlarmFlag = 1;
@@ -47,7 +67,7 @@ extern void Battery_Check(void)
             Battery.BatteryAlarmFlag = 0;
         }
 			
-		//过放保护，Battery overdischarge protect
+		/*过放保护，Battery overdischarge protect*/
         if (Battery.BatteryRealVal <= BAT_OVERDIS_VAL)
         {
             Battery.BatteryOverDischargeCnt++;
@@ -64,8 +84,8 @@ extern void Battery_Check(void)
 	}
     else
     {
-        if ((Battery.BatteryRealVal < BAT_ALARM_VAL) && (Battery.BatteryRealVal > BAT_CHARGE_VAL))//低于3.7v 且大于充电检测电压 BAT_CHG_VAL
-        {	
+        if ((Battery.BatteryRealVal < BAT_ALARM_VAL) && (Battery.BatteryRealVal > BAT_CHARGE_VAL))  /*低于3.7v 且大于充电检测电压 BAT_CHG_VAL*/
+        {
             Battery.BatteryAlarmFlag = 1;
         }
         else
@@ -73,10 +93,10 @@ extern void Battery_Check(void)
             Battery.BatteryAlarmFlag = 0;
         }
     }
-		
-    if (Battery.BatteryRealVal < BAT_CHARGE_VAL)  //on charge
-    { 
-        Battery.BatteryChargeState = 1; 
+
+    if (Battery.BatteryRealVal < BAT_CHARGE_VAL)  /*on charge*/
+    {
+        Battery.BatteryChargeState = 1;
     }
     else
     {
@@ -89,48 +109,50 @@ extern void Battery_Check(void)
 //BatteryCheck---->PB0
 extern void Battery_Check_Init(void)
 {
-    //先初PB0为模拟输入
-    RCC->APB2ENR  |= 1<<3;        //使能PORTB口时钟 
-    GPIOB->CRL    &= 0XFFFFFFF0;  //PB0	anolog输入
-    //通道8	 
-    RCC->APB2ENR  |= 1<<9;        //ADC1时钟使能	  
-    RCC->APB2RSTR |= 1<<9;        //ADC1复位
-    RCC->APB2RSTR &= ~(1<<9);     //复位结束	    
-    RCC->CFGR     &= ~(3<<14);    //分频因子清零	
+    /*先初PB0为模拟输入*/
+    RCC->APB2ENR  |= 1<<3;        /*使能PORTB口时钟*/
+    GPIOB->CRL    &= 0XFFFFFFF0;  /*PB0	anolog输入*/
+    /*通道8*/
+    RCC->APB2ENR  |= 1<<9;        /*ADC1时钟使能*/
+    RCC->APB2RSTR |= 1<<9;        /*ADC1复位*/
+    RCC->APB2RSTR &= ~(1<<9);     /*复位结束*/
+    RCC->CFGR     &= ~(3<<14);    /*分频因子清零*/
+    
     //SYSCLK/DIV2=12M ADC时钟设置为12M,ADC最大时钟不能超过14M!
-    //否则将导致ADC准确度下降! 
-    RCC->CFGR |= 2<<14;      	 
-    ADC1->CR1 &= 0XF0FFFF;        //工作模式清零
-    ADC1->CR1 |= 0<<16;           //独立工作模式  
-    ADC1->CR1 &= ~(1<<8);         //非扫描模式	  
-    ADC1->CR2 &= ~(1<<1);         //单次转换模式
-    ADC1->CR2 &= ~(7<<17);	   
-    ADC1->CR2 |= 7<<17;           //软件控制转换  
-    ADC1->CR2 |= 1<<20;           //使用用外部触发(SWSTART)!!!	必须使用一个事件来触发
-    ADC1->CR2 &= ~(1<<11);        //右对齐	 
-    ADC1->CR2 |= 1<<23;           //使能温度传感器
+    //否则将导致ADC准确度下降!
+    RCC->CFGR |= 2<<14;
+    ADC1->CR1 &= 0XF0FFFF;        /*工作模式清零*/
+    ADC1->CR1 |= 0<<16;           /*独立工作模式*/
+    ADC1->CR1 &= ~(1<<8);         /*非扫描模式*/
+    ADC1->CR2 &= ~(1<<1);         /*单次转换模式*/
+    ADC1->CR2 &= ~(7<<17);
+    ADC1->CR2 |= 7<<17;           /*软件控制转换*/
+    ADC1->CR2 |= 1<<20;           /*使用用外部触发(SWSTART)!!!	必须使用一个事件来触发*/
+    ADC1->CR2 &= ~(1<<11);        /*右对齐*/
+    ADC1->CR2 |= 1<<23;           /*使能温度传感器*/
 
     ADC1->SQR1 &= ~(0XF<<20);
-    ADC1->SQR1 &= 0<<20;          //1个转换在规则序列中 也就是只转换规则序列1 			   
+    ADC1->SQR1 &= 0<<20;          /*1个转换在规则序列中 也就是只转换规则序列1*/
+    
     //设置通道1的采样时间
-    ADC1->SMPR2 &= ~(7 << 3);     //通道1采样时间清空	  
-    ADC1->SMPR2 |= 7<<3;          //通道1  239.5周期,提高采样时间可以提高精确度	 
+    ADC1->SMPR2 &= ~(7 << 3);     /*通道1采样时间清空*/
+    ADC1->SMPR2 |= 7<<3;          /*通道1  239.5周期,提高采样时间可以提高精确度*/
 
-    ADC1->SMPR1 &= ~(7<<18);      //清除通道16原来的设置	 
-    ADC1->SMPR1 |= 7<<18;         //通道16  239.5周期,提高采样时间可以提高精确度	 
+    ADC1->SMPR1 &= ~(7<<18);      /*清除通道16原来的设置*/
+    ADC1->SMPR1 |= 7<<18;         /*通道16  239.5周期,提高采样时间可以提高精确度*/
 
-    ADC1->CR2 |= 1<<0;            //开启AD转换器	 
-    ADC1->CR2 |= 1<<3;            //使能复位校准  
-    while (ADC1->CR2 & (1<<3));   //等待校准结束 			 
-    //该位由软件设置并由硬件清除。在校准寄存器被初始化后该位将被清除。 		 
-    ADC1->CR2 |= 1<<2;            //开启AD校准	   
-    while (ADC1->CR2 & (1<<2));   //等待校准结束
-    //该位由软件设置以开始校准，并在校准结束时由硬件清除  
-  
-    Battery.BatteryTestVal          = 3.95;  //单位为v 电池实际电压  校准电压时修改
-    Battery.BatteryADInputVal       = 1.98;  //单位为v R15和R17连接处电压 校准电压时修改
-    Battery.BatteryADRefVal         = 3.26;  //单位为v 单片机供电电压   校准电压时修改
-    Battery.BatteryFactor           = Battery.BatteryTestVal / Battery.BatteryADInputVal;  //计算电压计算系数
+    ADC1->CR2 |= 1<<0;            /*开启AD转换器*/
+    ADC1->CR2 |= 1<<3;            /*使能复位校准*/
+    while (ADC1->CR2 & (1<<3));   /*等待校准结束*/
+    //该位由软件设置并由硬件清除。在校准寄存器被初始化后该位将被清除。
+    ADC1->CR2 |= 1<<2;            /*开启AD校准*/
+    while (ADC1->CR2 & (1<<2));   /*等待校准结束*/
+    //该位由软件设置以开始校准，并在校准结束时由硬件清除
+
+    Battery.BatteryTestVal          = 3.95;  /*单位为v 电池实际电压  校准电压时修改*/
+    Battery.BatteryADInputVal       = 1.98;  /*单位为v R15和R17连接处电压 校准电压时修改*/
+    Battery.BatteryADRefVal         = 3.26;  /*单位为v 单片机供电电压   校准电压时修改*/
+    Battery.BatteryFactor           = Battery.BatteryTestVal / Battery.BatteryADInputVal;  /*计算电压计算系数*/
     Battery.BatteryOverDischargeCnt = 0;
     
     printf("Batter voltage AD init ...\r\n");  
@@ -142,11 +164,11 @@ extern void Battery_Check_Init(void)
 extern u16 Get_ADC(u8 ch)
 {
     //设置转换序列	  		 
-    ADC1->SQR3 &= 0XFFFFFFE0;      //规则序列1 通道ch
-    ADC1->SQR3 |= ch;		  			    
-    ADC1->CR2  |= 1<<22;           //启动规则转换通道 
-    while (!(ADC1->SR & (1<<1)));  //等待转换结束	 	   
-    return ADC1->DR;               //返回adc值	
+    ADC1->SQR3 &= 0XFFFFFFE0;      /*规则序列1 通道ch*/
+    ADC1->SQR3 |= ch;
+    ADC1->CR2  |= 1<<22;           /*启动规则转换通道*/
+    while (!(ADC1->SR & (1<<1)));  /*等待转换结束*/
+    return ADC1->DR;               /*返回adc值*/
 }
 
 //获取通道ch的转换值，取times次,然后平均 

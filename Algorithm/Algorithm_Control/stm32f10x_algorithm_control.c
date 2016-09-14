f39693fe365de22074139b067d7fcb7ea09f96b0
maksyuki <myc20404401@sina.com>
2016-09-14 18:59:08 +0800

    Modify Control module
    1.modify Control module.

--- Algorithm/Algorithm_Control/stm32f10x_algorithm_control.c
+++ Algorithm/Algorithm_Control/stm32f10x_algorithm_control.c
@@ -183,259 +183,298 @@ uint8_t isAltLimit  = 0;
 /*悬停油门值相关因素有：电池电压*/
 /*Get a estimated value for hold throttle.It will have a direct affection on hover*/
 /*Battery voltage*/
-float estimateHoverThru(void){
+float estimateHoverThru(void)
+{
     float hoverHru = -0.55f;
 
     /*电池电压检测*/
     Battery.ADVal   = Get_Battery_AD();
     Battery.RealVal = Battery.Factor * (Battery.ADVal / 4096.0) * Battery.ADRefVal;  /*实际电压值计算*/
 
-    if(Battery.RealVal > 4.05){
-		hoverHru = -0.25f;
-	}else if(Battery.RealVal > 3.90){
-		hoverHru = -0.40f;
-	}else if(Battery.RealVal > 3.80){
-		hoverHru = -0.45f;
-	}else if(Battery.RealVal > 3.70){
-		hoverHru = -0.50f;
-	}else{
-		hoverHru = -0.55f;
-	}
-	
-	
-//	if(Battery.RealVal > 4.05){
-//		hoverHru = -0.05f;
-//	}else if(Battery.RealVal > 3.90){
-//		hoverHru = -0.10f;
-//	}else if(Battery.RealVal > 3.80){
-//		hoverHru = -0.15f;
-//	}else if(Battery.RealVal > 3.70){
-//		hoverHru = -0.20f;
-//	}else{
-//		hoverHru = -0.25f;
-//	}
-	
-	return hoverHru;
+    if (Battery.RealVal > 4.05)
+    {
+        hoverHru = -0.25f;
+    }
+    else if (Battery.RealVal > 3.90)
+    {
+        hoverHru = -0.40f;
+    }
+    else if (Battery.RealVal > 3.80)
+    {
+        hoverHru = -0.45f;
+    }
+    else if (Battery.RealVal > 3.70)
+    {
+        hoverHru = -0.50f;
+    }
+    else
+    {
+        hoverHru = -0.55f;
+    }
+
+    return hoverHru;
 }
 
+/*函数名：estimateMinThru()*/
+/*输入：无*/
+/*输出: 预估得到的最小油门值*/
+/*描述：预估最小油门值，根据机重、电池电量而定*/
+/*油门过小，下降速度过大时，导致失衡，例如快速下降时机身晃动厉害。再增加fuzzy control ，在油门小时用更大的姿态参数*/
+/*相关因素有：电池电压*/
+float estimateMinThru(void)
+{
+    float minThru = -0.55f;
 
-//函数名：estimateMinThru()
-//输入：无
-//输出: 预估得到的最小油门值
-//描述：预估最小油门值，根据机重、电池电量而定
-//油门过小，下降速度过大时，导致失衡，例如快速下降时机身晃动厉害。再增加fuzzy control ，在油门小时用更大的姿态参数
-//相关因素有：电池电压
-float estimateMinThru(void){
-	float minThru = -0.55f;
-	
-	//电池电压检测
-	Battery.ADVal  = Get_Battery_AD();
-	Battery.RealVal = Battery.Factor * (Battery.ADVal/4096.0) * Battery.ADRefVal;//实际电压 值计算
-	
-	if(Battery.RealVal > 4.05){
-		minThru = -0.30f;
-	}else if(Battery.RealVal > 3.90){
-		minThru = -0.40f;
-	}else{
-		minThru = -0.55f;
-	}
-	
-	return minThru;
+    /*电池电压检测*/
+    Battery.ADVal   = Get_Battery_AD();
+    Battery.RealVal = Battery.Factor * (Battery.ADVal / 4096.0) * Battery.ADRefVal;  /*实际电压值计算*/
+
+    if (Battery.RealVal > 4.05)
+    {
+        minThru = -0.30f;
+    }
+    else if (Battery.RealVal > 3.90)
+    {
+        minThru = -0.40f;
+    }
+    else
+    {
+        minThru = -0.55f;
+    }
+
+    return minThru;
 }
 
-//函数名：CtrlAlti()
-//输入：无
-//输出: 最终结果输出到全局变量thrustZSp
-//描述：控制高度，也就是高度悬停控制函数
-//only in climb rate mode and landind mode. now we don't work on manual mode
+/*函数名：CtrlAlti()*/
+/*输入：无*/
+/*输出: 最终结果输出到全局变量thrustZSp*/
+/*描述：控制高度，也就是高度悬停控制函数*/
+/*only in climb rate mode and landind mode. now we don't work on manual mode*/
 void CtrlAlti(void)
 {
-	float manThr=0,alt=0,velZ=0;
-	float altSp=0;
-	float posZVelSp=0;
-	float altSpOffset,altSpOffsetMax=0;
-	float dt=0,t=0;
-	static float tPrev=0,velZPrev=0;
-	float posZErr=0,velZErr=0,valZErrD=0;
-	float thrustXYSpLen=0,thrustSpLen=0;
-	float thrustXYMax=0;
-	float minThrust;
-	
-	//get dt		
-	//保证dt运算不能被打断，保持更新，否则dt过大，积分爆满。
-	if(tPrev==0){
-			tPrev=micros();
-			return;
-	}else{
-			t=micros();
-			dt=(t-tPrev) /1000000.0f;
-			tPrev=t;
-	}
-	
-	//only in climb rate mode and landind mode. now we don't work on manual mode
-	//手动模式不使用该高度控制算法
-	if(MANUAL == altCtrlMode || !FLY_ENABLE){
-		return;
-	}
-	
-	//--------------pos z ctrol---------------//
-	//get current alt 
-	alt=-nav.z;
-	//get desired move rate from stick
-	manThr=RC_DATA.THROTTLE / 1000.0f;
-	spZMoveRate= -dbScaleLinear(manThr-0.5f,0.5f,ALT_CTRL_Z_DB);	// scale to -1~1 . NED frame
-	spZMoveRate = spZMoveRate * ALT_VEL_MAX;	// scale to vel min max
+    float manThr          = 0;
+    float alt             = 0;
+    float velZ            = 0;
+    float altSp           = 0;
+    float posZVelSp       = 0;
+    float altSpOffset;
+    float altSpOffsetMax  = 0;
+    float dt              = 0;
+    float t               = 0;
+    static float tPrev    = 0;
+    static float velZPrev = 0;
+    float posZErr         = 0;
+    float velZErr         = 0;
+    float valZErrD        = 0;
+    float thrustXYSpLen   = 0;
+    float thrustSpLen     = 0;
+    float thrustXYMax     = 0;
+    float minThrust;
 
-	//get alt setpoint in CLIMB rate mode
-	altSp 	=-nav.z;						//only alt is not in ned frame.
-	altSp  -= spZMoveRate * dt;	 
-	//limit alt setpoint
-	altSpOffsetMax=ALT_VEL_MAX / alt_PID.P * 2.0f;
-	altSpOffset = altSp-alt; 
-	if( altSpOffset > altSpOffsetMax){
-		altSp=alt +  altSpOffsetMax;
-	}else if( altSpOffset < -altSpOffsetMax){
-		altSp=alt - altSpOffsetMax;
-	}
+    /*get dt*/
+    /*保证dt运算不能被打断，保持更新，否则dt过大，积分爆满*/
+    if (tPrev == 0)
+    {
+        tPrev = micros();
+        return;
+    }
+    else
+    {
+        t     = micros();
+        dt    = (t-tPrev) /1000000.0f;
+        tPrev = t;
+    }
 
-	//限高
-	if(isAltLimit)
-	{
-		if(altSp - altLand > ALT_LIMIT)
-		{
-				altSp=altLand+ALT_LIMIT;
-				spZMoveRate=0;
-		}
-	}
-	
-	// pid and feedforward control . in ned frame
-	posZErr= -(altSp - alt);
-	posZVelSp = posZErr * alt_PID.P + spZMoveRate * ALT_FEED_FORWARD;
-	//consider landing mode
-	if(altCtrlMode==LANDING)
-		posZVelSp = LAND_SPEED;
-	
-	//获取一个预估的Z轴悬停基准值，相关因素有电池电压
-	//get hold throttle. give it a estimated value
-	if(zIntReset){
-		thrustZInt = estimateHoverThru();
-		zIntReset = 0;
-	}
-	
-	velZ=nav.vz;	
-	velZErr = posZVelSp - velZ;
-	valZErrD = (spZMoveRate - velZ) * alt_PID.P - (velZ - velZPrev) / dt;	//spZMoveRate is from manual stick vel control
-	velZPrev=velZ;
-	
-	thrustZSp= velZErr * alt_vel_PID.P + valZErrD * alt_vel_PID.D + thrustZInt;	//in ned frame. thrustZInt contains hover thrust
-	
-	//限制最小下降油门
-	minThrust = estimateMinThru();
-	if(altCtrlMode!=LANDING){
-		if (-thrustZSp < minThrust){
-			thrustZSp = -minThrust; 
-		} 
-	}
-	
-	//与动力分配相关	testing
-	satXY=0;
-	satZ=0;
-	thrustXYSp[0]= sinf(RC_DATA.ROOL * M_PI_F /180.0f) ;//目标角度转加速度
-	thrustXYSp[1]= sinf(RC_DATA.PITCH * M_PI_F /180.0f) ; 	//归一化
-	thrustXYSpLen= sqrtf(thrustXYSp[0] * thrustXYSp[0] + thrustXYSp[1] * thrustXYSp[1]);
-	//limit tilt max
-	if(thrustXYSpLen >0.01f )
-	{
-		thrustXYMax=-thrustZSp * tanf(TILT_MAX);
-		if(thrustXYSpLen > thrustXYMax)
-		{
-				float k=thrustXYMax / thrustXYSpLen;
-				thrustXYSp[1] *= k;
-				thrustXYSp[0] *= k;
-				satXY=1;
-				thrustXYSpLen= sqrtf(thrustXYSp[0] * thrustXYSp[0] + thrustXYSp[1] * thrustXYSp[1]);
-		}
-		
-	}
-	//limit max thrust!! 
-	thrustSpLen=sqrtf(thrustXYSpLen * thrustXYSpLen + thrustZSp * thrustZSp);
-	if(thrustSpLen > THR_MAX)
-	{
-			if(thrustZSp < 0.0f)	//going up
-			{
-						if (-thrustZSp > THR_MAX) 
-						{
-								/* thrust Z component is too large, limit it */
-								thrustXYSp[0] = 0.0f;
-								thrustXYSp[1] = 0.0f;
-								thrustZSp = -THR_MAX;
-								satXY = 1;
-								satZ = 1;
+    /*only in climb rate mode and landind mode. now we don't work on manual mode*/
+    /*手动模式不使用该高度控制算法*/
+    if (MANUAL == altCtrlMode || !FLY_ENABLE)
+    {
+        return;
+    }
 
-							} 
-							else {
-								float k = 0;
-								/* preserve thrust Z component and lower XY, keeping altitude is more important than position */
-								thrustXYMax = sqrtf(THR_MAX * THR_MAX- thrustZSp * thrustZSp);
-								k=thrustXYMax / thrustXYSpLen;
-								thrustXYSp[1] *=k;
-								thrustXYSp[0] *= k;
-								satXY=1;
-							}
-			}
-			else {		//going down
-							/* Z component is negative, going down, simply limit thrust vector */
-							float k = THR_MAX / thrustSpLen;
-							thrustZSp *= k;
-							thrustXYSp[1] *=k;
-							thrustXYSp[0] *= k;
-							satXY = 1;
-							satZ = 1;
-						}
-		
-	} 
-	rollSp= asinf(thrustXYSp[0]) * 180.0f /M_PI_F;
-	pitchSp = asinf(thrustXYSp[1]) * 180.0f /M_PI_F;				
-	
-	
-	// if saturation ,don't integral
-	if(!satZ )//&& fabs(thrustZSp)<THR_MAX
-	{
-			thrustZInt += velZErr * alt_vel_PID.I * dt;
-			if (thrustZInt > 0.0f)
-							thrustZInt = 0.0f;
-	}
+    /*--------------pos z ctrol---------------*/
+    /*get current alt*/
+    alt = -nav.z;
+
+    /*get desired move rate from stick*/
+    manThr      = RC_DATA.THROTTLE / 1000.0f;
+    spZMoveRate = -dbScaleLinear(manThr - 0.5f, 0.5f, ALT_CTRL_Z_DB);  /*scale to -1~1 . NED frame*/
+    spZMoveRate = spZMoveRate * ALT_VEL_MAX;                           /*scale to vel min max*/
+
+    /*get alt setpoint in CLIMB rate mode*/
+    altSp   = -nav.z;                        /*only alt is not in ned frame.*/
+    altSp  -= spZMoveRate * dt;
+
+    /*limit alt setpoint*/
+    altSpOffsetMax = ALT_VEL_MAX / alt_PID.P * 2.0f;
+    altSpOffset    = altSp - alt;
+
+    if (altSpOffset > altSpOffsetMax)
+    {
+        altSp = alt + altSpOffsetMax;
+    }
+    else if (altSpOffset < -altSpOffsetMax)
+    {
+        altSp = alt - altSpOffsetMax;
+    }
+
+    /*限高*/
+    if (isAltLimit)
+    {
+        if (altSp - altLand > ALT_LIMIT)
+        {
+            altSp       = altLand + ALT_LIMIT;
+            spZMoveRate = 0;
+        }
+    }
+
+    /*pid and feedforward control . in ned frame*/
+    posZErr   = -(altSp - alt);
+    posZVelSp = posZErr * alt_PID.P + spZMoveRate * ALT_FEED_FORWARD;
+
+    /*consider landing mode*/
+    if (altCtrlMode == LANDING)
+    {
+        posZVelSp = LAND_SPEED;
+    }
+
+    /*获取一个预估的Z轴悬停基准值，相关因素有电池电压*/
+    /*get hold throttle. give it a estimated value*/
+    if (zIntReset)
+    {
+        thrustZInt = estimateHoverThru();
+        zIntReset  = 0;
+    }
+
+    velZ     = nav.vz;
+    velZErr  = posZVelSp - velZ;
+    valZErrD = (spZMoveRate - velZ) * alt_PID.P - (velZ - velZPrev) / dt;         /*spZMoveRate is from manual stick vel control*/
+    velZPrev = velZ;
+
+    thrustZSp = velZErr * alt_vel_PID.P + valZErrD * alt_vel_PID.D + thrustZInt;  /*in ned frame. thrustZInt contains hover thrust*/
+
+    /*限制最小下降油门*/
+    minThrust = estimateMinThru();
+    if (altCtrlMode != LANDING)
+    {
+        if (-thrustZSp < minThrust)
+        {
+            thrustZSp = -minThrust;
+        }
+    }
+
+    /*与动力分配相关	testing*/
+    satXY = 0;
+    satZ  = 0;
+    thrustXYSp[0] = sinf(RC_DATA.ROOL * M_PI_F /180.0f);    /*目标角度转加速度*/
+    thrustXYSp[1] = sinf(RC_DATA.PITCH * M_PI_F /180.0f);   /*归一化*/
+    thrustXYSpLen = sqrtf(thrustXYSp[0] * thrustXYSp[0] + thrustXYSp[1] * thrustXYSp[1]);
+
+    /*limit tilt max*/
+    if (thrustXYSpLen > 0.01f)
+    {
+        thrustXYMax = -thrustZSp * tanf(TILT_MAX);
+        
+        if (thrustXYSpLen > thrustXYMax)
+        {
+            float k = thrustXYMax / thrustXYSpLen;
+            thrustXYSp[1] *= k;
+            thrustXYSp[0] *= k;
+            satXY          = 1;
+            thrustXYSpLen  = sqrtf(thrustXYSp[0] * thrustXYSp[0] + thrustXYSp[1] * thrustXYSp[1]);
+        }
+    }
+
+    /*limit max thrust!!*/
+    thrustSpLen = sqrtf(thrustXYSpLen * thrustXYSpLen + thrustZSp * thrustZSp);
+
+    if (thrustSpLen > THR_MAX)
+    {
+        if (thrustZSp < 0.0f)   /*going up*/
+        {
+            if (-thrustZSp > THR_MAX)
+            {
+                /* thrust Z component is too large, limit it */
+                thrustXYSp[0] = 0.0f;
+                thrustXYSp[1] = 0.0f;
+                thrustZSp     = -THR_MAX;
+                satXY         = 1;
+                satZ          = 1;
+            }
+            else
+            {
+                float k = 0;
+
+                /*preserve thrust Z component and lower XY, keeping altitude is more important than position*/
+                thrustXYMax = sqrtf(THR_MAX * THR_MAX - thrustZSp * thrustZSp);
+                k = thrustXYMax / thrustXYSpLen;
+                thrustXYSp[1] *= k;
+                thrustXYSp[0] *= k;
+                satXY = 1;
+            }
+        }
+        else 
+        {       /*going down*/
+                /*Z component is negative, going down, simply limit thrust vector*/
+                float k        = THR_MAX / thrustSpLen;
+                thrustZSp     *= k;
+                thrustXYSp[1] *= k;
+                thrustXYSp[0] *= k;
+                satXY          = 1;
+                satZ           = 1;
+        }
+    }
+
+    rollSp  = asinf(thrustXYSp[0]) * 180.0f / M_PI_F;
+    pitchSp = asinf(thrustXYSp[1]) * 180.0f / M_PI_F;
+
+    /*if saturation ,don't integral*/
+    if (!satZ)  /*&& fabs(thrustZSp)<THR_MAX*/
+    {
+        thrustZInt += velZErr * alt_vel_PID.I * dt;
+        if (thrustZInt > 0.0f)
+        {
+            thrustZInt = 0.0f;
+        }				
+    }
 }
- 
 
-//函数名：CtrlMotor()
-//输入：无
-//输出: 4个电机的PWM输出
-//描述：输出PWM，控制电机，本函数会被主循环中100Hz循环调用
+/*函数名：CtrlMotor()*/
+/*输入：无*/
+/*输出: 4个电机的PWM输出*/
+/*描述：输出PWM，控制电机，本函数会被主循环中100Hz循环调用*/
 void CtrlMotor(void)
 {
-		float  cosTilt = imu.accb[2] / ONE_G;
-	
-		if(altCtrlMode==MANUAL)
-		{
-			DIF_ACC.Z =  imu.accb[2] - ONE_G;
-			Thro = RC_DATA.THROTTLE;
-			cosTilt=imu.DCMgb[2][2];
-			Thro=Thro/cosTilt;
-		}else{
-			Thro=(-thrustZSp) * 1000;// /imu.DCMgb[2][2];  //倾角补偿后效果不错，有时过猛
-			if(Thro>1000)
-				Thro=1000;
-		}
-		
-		//将输出值融合到四个电机 
-		Motor[2] = (int16_t)(Thro - Pitch - Roll - Yaw );    //M3  
-		Motor[0] = (int16_t)(Thro + Pitch + Roll - Yaw );    //M1
-		Motor[3] = (int16_t)(Thro - Pitch + Roll + Yaw );    //M4 
-		Motor[1] = (int16_t)(Thro + Pitch - Roll + Yaw );    //M2
-	
-   	if((FLY_ENABLE!=0)) 
-			MotorPwmFlash(Motor[0],Motor[1],Motor[2],Motor[3]);   
-		else                  
-			MotorPwmFlash(0,0,0,0); 
+    float cosTilt = imu.accb[2] / ONE_G;
+
+    if (altCtrlMode == MANUAL)
+    {
+        DIF_ACC.Z = imu.accb[2] - ONE_G;
+        Thro      = RC_DATA.THROTTLE;
+        cosTilt   = imu.DCMgb[2][2];
+        Thro      = Thro / cosTilt;
+    }
+    else
+    {
+        Thro = (-thrustZSp) * 1000;  /*/imu.DCMgb[2][2];  //倾角补偿后效果不错，有时过猛*/
+        if (Thro > 1000)
+        {
+            Thro = 1000;
+        }
+    }
+
+    /*将输出值融合到四个电机*/
+    Motor[2] = (int16_t)(Thro - Pitch - Roll - Yaw );  /*M3*/
+    Motor[0] = (int16_t)(Thro + Pitch + Roll - Yaw );  /*M1*/
+    Motor[3] = (int16_t)(Thro - Pitch + Roll + Yaw );  /*M4*/
+    Motor[1] = (int16_t)(Thro + Pitch - Roll + Yaw );  /*M2*/
+
+   	if (FLY_ENABLE != 0)
+    {
+        MotorPwmFlash(Motor[0], Motor[1], Motor[2], Motor[3]);
+    }
+    else
+    {
+        MotorPwmFlash(0,0,0,0);
+    }
 }

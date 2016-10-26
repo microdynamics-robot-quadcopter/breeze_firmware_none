6ad6c457fc2a2f3d2b2b4ff7bca3ef1c9ead6da3
maksyuki <myc20404401@sina.com>
2016-10-26 10:21:03 +0800

    Call the functions from the modules

--- keil/User/main.c
+++ keil/User/main.c
@@ -2,6 +2,7 @@
 #include "stm32f10x_driver_pwm.h"
 #include "stm32f10x_driver_tim.h"
 #include "stm32f10x_driver_iic.h"
+#include "stm32f10x_driver_flash.h"
 #include "stm32f10x_driver_delay.h"
 #include "stm32f10x_driver_usart.h"
 #include "stm32f10x_driver_eeprom.h"
@@ -11,28 +12,38 @@
 #include "stm32f10x_system_battery.h"
 #include "stm32f10x_system_mpu6050.h"
 #include "stm32f10x_system_nrf24l01.h"
+#include "stm32f10x_algorithm_bar.h"
 #include "stm32f10x_algorithm_imu.h"
+#include "stm32f10x_algorithm_flight.h"
 #include "stm32f10x_algorithm_control.h"
+#include "stm32f10x_it.h"
+
+/*software counter*/
+uint16_t BatCnt = 0;
 
 void Hardware_Init(void)
 {
+    SystemClock_HSE(9);
+    CycleCounter_Init();                     /*Init cycle counter*/
+    SysTick_Config(SystemCoreClock / 1000);  /*SysTick开启系统tick定时器并初始化其中断，中断溢出时间为1ms，写法是固定的*/
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-    usart_init(115200);
-    TIM4_Init(4999, 7199);
-    FLASH_Unlock();
+    usart_Init(115200);
+    TIM4_Init(1000, SysClock);
+    //FLASH_Unlock();
+    STMFLASH_Unlock();
     LoadParamsFromEEPROM();
-    delay_init();
-    led_init();
+    //delay_init();
+    LED_Init();
     PWM_Init();
     Battery_CheckInit();
     IIC_Init();
     MPU6050_Init();
-    NRF24L01_INIT();
+    NRF24L01_Init();
     Battery_Check();
     MS5611_Init();
     IMU_Init();
     altCtrlMode = MANUAL;
-//    MS5611_WaitBaroInitOffset(); 总是得不到合适的时间导致死循环
+    MS5611_WaitBaroInitOffset();
 }
 
 int main(void)
@@ -40,30 +51,75 @@ int main(void)
 //    u8 len = 0;
 //    u8 i;
 //    u16 time = 0;
-    Hardware_Init();   
-//    PWM_MotorFlash(200, 200, 200, 200);
+    Hardware_Init();
+//  PWM_MotorFlash(200, 200, 200, 200);
    
-    while (1)
-    {
-        //led_test(1);
-        NRF_Irq();
-        ProcessDataFromNRF();
-    }
-    
 //    while (1)
 //    {
-//        if (loop100HZCnt >= 10)
-//        {
-//            loop100HZCnt = 0;
-//        }
-//        
+//        NRF_Irq();
+//        ProcessDataFromNRF();
 //    }
-    
+
+    LedA_On;
+    while(1)
+    {
+        if (loop100HzFlag)
+        {
+            loop100HzFlag = 0;
+
+            IMU_SO3Thread();
+            accUpdated = 1;
+            MS5611_Thread();
+
+            if (imuCaliFlag)  /*为1表示进行校准*/
+            {
+                if (IMU_Calibrate())
+                {
+                    imuCaliFlag              = 0;
+                    gParamsSaveEEPROMRequest = 1;
+                    imu.caliPass             = 1;
+                    LedB_On;
+                }
+            }
+            ControlAttiRate();
+            ControlMotor();
+        }
+
+        NRF_Irq();
+
+        if (loop50HzFlag)
+        {
+            loop50HzFlag = 0;
+
+            ProcessDataFromNRF();
+            FlightStateSet();
+            AltitudeCombineThread();
+            ControlAlti();
+            ControlAttiAng();
+        }
+
+        if (loop10HzFlag)
+        {
+            loop10HzFlag = 0;
+
+            if ((++BatCnt) * 100 >= BAT_CHECK_PERIOD)
+            {
+                BatCnt = 0;
+                Battery_Check();
+            }
+
+            if (gParamsSaveEEPROMRequest)
+            {
+                gParamsSaveEEPROMRequest = 0;
+                SaveParamsToEEPROM();
+            }
+        }
+    }
 //    while (1)
 //    {
-//        led_test(1);
+//        LED_test(1);
 //        delay_ms(1000);
-//        led_test(0);
+//        LED_test(0);
 //        delay_ms(1000);
 //        if (USART_RX_STA & 0x8000)
 //        {

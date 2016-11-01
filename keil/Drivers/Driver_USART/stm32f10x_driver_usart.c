4c9fe9b3bcf6ba0602c43b2a0d23247f502bef43
maksyuki <myc20404401@sina.com>
2016-11-01 21:06:54 +0800

    Add some functions to support the MCU communicate with PC

--- keil/Drivers/Driver_USART/stm32f10x_driver_usart.c
+++ keil/Drivers/Driver_USART/stm32f10x_driver_usart.c
@@ -138,3 +138,64 @@ void USART1_IRQHandler(void)
 }
 
 #endif
+
+void UART1_Put_Char(unsigned char DataToSend)
+{
+    UartBuf_WD(&UartTxbuf, DataToSend);            /*将待发送数据放在环形缓冲数组中*/
+    USART_ITConfig(USART1, USART_IT_TXE, ENABLE);  /*启动发送中断开始啪啪啪发送缓冲中的数据*/
+}
+
+uint8_t Uart1_Put_Char(unsigned char DataToSend)
+{
+    UartBuf_WD(&UartTxbuf, DataToSend);            /*将待发送数据放在环形缓冲数组中*/
+    USART_ITConfig(USART1, USART_IT_TXE, ENABLE);  /*启动发送中断开始啪啪啪发送缓冲中的数据*/
+    return DataToSend;
+}
+
+/*环形数组结构体实例化两个变量*/
+UartBuf UartTxbuf;  /*环形发送结构体*/
+UartBuf UartRxbuf;  /*环形接收结构体*/
+
+unsigned char rx_buffer[RX_BUFFER_SIZE];
+unsigned char tx_buffer[TX_BUFFER_SIZE];
+
+/*读取环形数据中的一个字节*/
+uint8_t UartBuf_RD(UartBuf *Ringbuf)
+{
+    uint8_t temp;
+    temp = Ringbuf->pbuf[Ringbuf->Rd_Indx&Ringbuf->Mask];  /*数据长度掩码很重要，这是决定数据环形的关键*/
+    Ringbuf->Rd_Indx++;                                    /*读取完成一次，读指针加1，为下一次 读取做准备*/
+    return temp;
+}
+
+/*将一个字节写入一个环形结构体中*/
+void UartBuf_WD(UartBuf *Ringbuf,uint8_t DataIn)
+{
+    Ringbuf->pbuf[Ringbuf->Wd_Indx&Ringbuf->Mask] = DataIn; /*数据长度掩码很重要，这是决定数据环形的关键*/
+    Ringbuf->Wd_Indx++;                                     /*写完一次，写指针加1，为下一次写入做准备*/
+}
+
+/*环形数据区的可用字节长度，当写指针写完一圈，追上了读指针*/
+/*那么证明数据写满了，此时应该增加缓冲区长度，或者缩短外围数据处理时间*/
+uint16_t UartBuf_Cnt(UartBuf *Ringbuf)
+{
+    return (Ringbuf->Wd_Indx - Ringbuf->Rd_Indx) & Ringbuf->Mask;  /*数据长度掩码很重要，这是决定数据环形的关键*/
+}
+
+void UartBufClear(UartBuf *Ringbuf)
+{
+    Ringbuf->Rd_Indx = Ringbuf->Wd_Indx;
+}
+
+void UartSendBuffer(uint8_t *dat, uint8_t len)
+{
+    uint8_t i;
+    for (i = 0; i < len; i++)
+    {
+        UartBuf_WD(&UartTxbuf, *dat);
+        dat++;
+    }
+    USART_ITConfig(USART1, USART_IT_TXE, ENABLE);   /*启动发送中断开始啪啪啪发送缓冲中的数据*/
+}
+
+volatile uint8_t Udatatmp;  /*串口接收临时数据字节*/
--- keil/Drivers/Driver_USART/stm32f10x_driver_usart.h
+++ keil/Drivers/Driver_USART/stm32f10x_driver_usart.h
@@ -23,6 +23,35 @@ History:     none
 extern u8 USART_RX_BUF[USART_REC_LEN];  /*接收缓冲，最大USART_REC_LEN个字节，最后一个字节为换行符*/
 extern u16 USART_RX_STA;                /*接收状态标记*/
 
+/*USART Receiver buffer*/
+#define RX_BUFFER_SIZE   128
+#define TX_BUFFER_SIZE   128
+
+extern unsigned char rx_buffer[RX_BUFFER_SIZE];
+extern unsigned char tx_buffer[TX_BUFFER_SIZE];
+
+typedef struct
+{
+    uint16_t volatile Wd_Indx;
+    uint16_t volatile Rd_Indx;
+    uint16_t Mask;
+    uint8_t *pbuf;
+}UartBuf;
+
+extern UartBuf UartTxbuf;/*环形发送结构体*/
+extern UartBuf UartRxbuf;/*环形接收结构体*/
+
 extern void usart_Init(u32 bound);      /*USART_Init()在库函数中有*/
+
+extern uint8_t flyLogF;
+
+void UartBufClear(UartBuf *Ringbuf);
+void UART1_Put_Char(unsigned char DataToSend);
+uint8_t Uart1_Put_Char(unsigned char DataToSend);
+
+extern uint8_t UartBuf_RD(UartBuf *Ringbuf);
+extern uint16_t UartBuf_Cnt(UartBuf *Ringbuf);
+extern void UartBuf_WD(UartBuf *Ringbuf,uint8_t DataIn);
+void UartSendBuffer(uint8_t *dat, uint8_t len);
 
 #endif

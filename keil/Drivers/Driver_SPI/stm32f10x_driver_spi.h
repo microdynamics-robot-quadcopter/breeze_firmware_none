d2d8364cf4cab09e6846077cebbeb9c7b053bb45
maksyuki <myc20404401@sina.com>
2016-11-30 11:16:36 +0800

    Modify the comments in th SPI module

--- keil/Drivers/Driver_SPI/stm32f10x_driver_spi.h
+++ keil/Drivers/Driver_SPI/stm32f10x_driver_spi.h
@@ -1,22 +1,28 @@
 /*******************************************************************************
-Copyright (C), 2016-2016, Team MicroDynamics. 
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
 
 Filename:    stm32f10x_driver_spi.h
 Author:      maksyuki
-Version:     1.0
-Date:        2016.8.14
-Description: declare the spi bus operation function
+Version:     0.1.0.20161231_release
+Create date: 2016.8.14
+Description: declare the SPI bus operation function
 Others:      none
 Function List:
-             1. extern void SPI_INIT(void);
-             2. extern u8 SPI_RW(u8 data);
-History:     none
+             1. void SPI_INIT(void);
+             2. u8 SPI_RW(u8 data);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2016.11.30  modify the module
 *******************************************************************************/
 
 #ifndef __STM32F10X_DRIVER_SPI_H__
 #define __STM32F10X_DRIVER_SPI_H__
 
 #include "stm32f10x.h"
+
 /*
 6.2.4G:	CE:   PA15
         CSN:  PA4

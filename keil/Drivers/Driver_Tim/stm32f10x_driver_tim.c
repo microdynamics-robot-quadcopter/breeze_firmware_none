0caffdf1d9a2aa42141bcf6bce74de5011a332d2
maksyuki <myc20404401@sina.com>
2016-12-03 16:37:18 +0800

    Modify the comments in the TIM module

--- keil/Drivers/Driver_Tim/stm32f10x_driver_tim.c
+++ keil/Drivers/Driver_Tim/stm32f10x_driver_tim.c
@@ -1,15 +1,20 @@
 /*******************************************************************************
-Copyright (C), 2016-2016, Team MicroDynamics.
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
 
 Filename:    stm32f10x_driver_tim.c
 Author:      maksyuki
-Version:     1.0
-Date:        2016.8.17
+Version:     0.1.0.20161231_release
+Create date: 2016.8.17
 Description: implement the timer set operation
 Others:      none
 Function List:
-             1. extern void TIM4_Init(u16 arr, u16 psc);
-History:     none
+             1. void TIM4_Init(u16 arr, u16 psc);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2016.12.03  modify the module
 *******************************************************************************/
 
 #include "stm32f10x_driver_tim.h"
@@ -30,7 +35,7 @@ void TIM4_Init(u16 arr, u16 psc)
     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
     NVIC_InitTypeDef NVIC_InitStructure;
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-    TIM_DeInit(TIM4);  /*复位定时器4*/
+    TIM_DeInit(TIM4);
     TIM_TimeBaseStructure.TIM_Period        = arr;
     TIM_TimeBaseStructure.TIM_Prescaler     = psc - 1;
     TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
@@ -46,24 +51,24 @@ void TIM4_Init(u16 arr, u16 psc)
     TIM_Cmd(TIM4, ENABLE);
 }
 
-/*每1ms申请一次中断*/
+/* Request interrupts per millisecond */
 void TIM4_IRQHandler(void)
 {
     if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
     {
         TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
 
-        if (++loop100HzCnt * 100 >= 1000)  /*溢出时间10ms*/
+        if (++loop100HzCnt * 100 >= 1000)  /* Overflow time is 10ms */
         {
             loop100HzCnt  = 0;
             loop100HzFlag = 1;
         }
-        if (++loop50HzCnt * 50 >= 1000)    /*溢出时间20ms*/
+        if (++loop50HzCnt * 50 >= 1000)    /* Overflow time is 20ms */
         {
             loop50HzCnt  = 0;
             loop50HzFlag = 1;
         }
-        if (++loop10HzCnt * 10 >= 1000)    /*溢出时间100ms*/
+        if (++loop10HzCnt * 10 >= 1000)    /* Overflow time is 100ms */
         {
             loop10HzCnt  = 0;
             loop10HzFlag = 1;
--- keil/Drivers/Driver_Tim/stm32f10x_driver_tim.h
+++ keil/Drivers/Driver_Tim/stm32f10x_driver_tim.h
@@ -1,15 +1,20 @@
 /*******************************************************************************
-Copyright (C), 2016-2016, Team MicroDynamics.
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
 
 Filename:    stm32f10x_driver_tim.h
 Author:      maksyuki
-Version:     1.0
-Date:        2016.8.17
+Version:     0.1.0.20161231_release
+Create date: 2016.8.17
 Description: declare the timer set operation
 Others:      none
 Function List:
-             1. extern void TIM4_Init(u16 arr, u16 psc);
-History:     none
+             1. void TIM4_Init(u16 arr, u16 psc);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2016.12.03  modify the module
 *******************************************************************************/
 
 #ifndef __STM32F10X_DRIVER_TIM_H__
@@ -17,18 +22,18 @@ History:     none
 
 #include "stm32f10x.h"
 
-/*控制算法主程序定时器中断变量*/
+/* The interrupt variables of timer for main control */
 extern volatile uint16_t loop10HzCnt;
 extern volatile uint16_t loop20HzCnt;
 extern volatile uint16_t loop50HzCnt;
 extern volatile uint16_t loop100HzCnt;
 
-/*控制算法主程序定时器中断标志*/
+/* The interrupt flags of timer for main control */
 extern uint8_t loop10HzFlag;
 extern uint8_t loop20HzFlag;
 extern uint8_t loop50HzFlag;
 extern uint8_t loop100HzFlag;
 
-extern void TIM4_Init(u16 arr, u16 psc); /*TIM4初始化*/
+extern void TIM4_Init(u16 arr, u16 psc); /* TIM4 initialization */
 
 #endif

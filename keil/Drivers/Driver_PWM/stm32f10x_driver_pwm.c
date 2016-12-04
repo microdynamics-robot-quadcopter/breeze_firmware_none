557102182dd262e58f80ad6d743aed2a0ba8a7f1
maksyuki <myc20404401@sina.com>
2016-12-04 08:55:20 +0800

    Modify the comments in the PWM module

--- keil/Drivers/Driver_PWM/stm32f10x_driver_pwm.c
+++ keil/Drivers/Driver_PWM/stm32f10x_driver_pwm.c
@@ -1,35 +1,29 @@
 /*******************************************************************************
-Copyright (C), 2016-2016, Team MicroDynamics. 
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
 
 Filename:    stm32f10x_driver_pwm.c
 Author:      maksyuki
-Version:     1.0
-Date:        2016.8.4
+Version:     0.1.0.20161231_release
+Create date: 2016.8.4
 Description: implement the PWM operation function
 Others:      none
 Function List:
-             1. extern void PWM_Init(void);
-             2. extern void PWM_MotoFlash(int16_t MOTO1_PWM, int16_t MOTO2_PWM, int16_t MOTO3_PWM, int16_t MOTO4_PWM);
-History:     none
+             1. void PWM_Init(void);
+             2. void PWM_MotorFlash(int16_t MOTO1_PWM, int16_t MOTO2_PWM,
+                                    int16_t MOTO3_PWM, int16_t MOTO4_PWM);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2016.12.04  modify the module
 *******************************************************************************/
 
 #include "stm32f10x_driver_pwm.h"
 
-/*******************************************************************************
-Function:       void PWM_Init(void)
-Description:    initializes the PWM
-Calls:          
-Called By:      
-Table Accessed: none
-Table Updated:  none
-Input:          none
-Output:         none
-Return:         none
-Others:         none
-*******************************************************************************/
 void PWM_Init(void)
 {
-    uint16_t                 PrescalerValue = 0;    /*Control the frequency rate of PWM*/
+    uint16_t                 PrescalerValue = 0;    /* Control the frequency rate of PWM */
     GPIO_InitTypeDef         GPIO_InitStructure;
     TIM_OCInitTypeDef        TIM_OCInitStructure;
     TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
@@ -37,24 +31,23 @@ void PWM_Init(void)
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
-    /*Set GPIO function*/
+    /* Set GPIO function */
     GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
     GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    /*Reset timer*/
     TIM_DeInit(TIM2);
 
-    /*Configure timer*/
+    /* Configure timer */
     PrescalerValue = (uint16_t) (SystemCoreClock / 24000000) - 1;
-    TIM_TimeBaseStructure.TIM_Period        = 999;		           /*The upper limit of counter*/	
-    TIM_TimeBaseStructure.TIM_Prescaler     = PrescalerValue;	   /*The timer fractional frequency of PWM*/
+    TIM_TimeBaseStructure.TIM_Period        = 999;                 /* The upper limit of counter */
+    TIM_TimeBaseStructure.TIM_Prescaler     = PrescalerValue;      /* The timer fractional frequency of PWM */
     TIM_TimeBaseStructure.TIM_ClockDivision = 0;
     TIM_TimeBaseStructure.TIM_CounterMode   = TIM_CounterMode_Up;
     TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
 
-    /*Configure the model of the TIM2 is PWM model*/
+    /* Configure the model of the TIM2 is PWM */
     TIM_OCStructInit(&TIM_OCInitStructure);
     TIM_OCInitStructure.TIM_OCMode      = TIM_OCMode_PWM1;
     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
@@ -71,22 +64,9 @@ void PWM_Init(void)
     TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable);
     TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable);
 
-    /*Start timer*/
     TIM_Cmd(TIM2, ENABLE);
 }
 
-/*******************************************************************************
-Function:       void PWM_MotorFlash(u16 MOTO1_PWM, u16 MOTO2_PWM, u16 MOTO3_PWM, u16 MOTO4_PWM)
-Description:    reflash the PWM output
-Calls:          
-Called By:      
-Table Accessed: none
-Table Updated:  none
-Input:          
-Output:         none
-Return:         none
-Others:         none
-*******************************************************************************/
 void PWM_MotorFlash(int16_t MOTO1_PWM, int16_t MOTO2_PWM, int16_t MOTO3_PWM, int16_t MOTO4_PWM)
 {
     if (MOTO1_PWM < 0) MOTO1_PWM = 0;
--- keil/Drivers/Driver_PWM/stm32f10x_driver_pwm.h
+++ keil/Drivers/Driver_PWM/stm32f10x_driver_pwm.h
@@ -1,16 +1,22 @@
 /*******************************************************************************
-Copyright (C), 2016-2016, Team MicroDynamics.
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
 
 Filename:    stm32f10x_driver_pwm.h
 Author:      maksyuki
-Version:     1.0
-Date:        2016.8.4
+Version:     0.1.0.20161231_release
+Create date: 2016.8.4
 Description: declare the PWM operation function
 Others:      none
 Function List:
-             1. extern void PWM_Init(void);
-             2. extern void PWM_Flash(int16_t MOTO1_PWM, int16_t MOTO2_PWM, int16_t MOTO3_PWM, int16_t MOTO4_PWM);
-History:     none
+             1. void PWM_Init(void);
+             2. void PWM_MotorFlash(int16_t MOTO1_PWM, int16_t MOTO2_PWM,
+                                    int16_t MOTO3_PWM, int16_t MOTO4_PWM);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2016.12.04  modify the module
 *******************************************************************************/
 
 #ifndef __STM32F10X_DRIVER_PWM_H__

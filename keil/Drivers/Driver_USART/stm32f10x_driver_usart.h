539649babe2386b752f0e4a9d7d280041451fdc6
maksyuki <myc20404401@sina.com>
2016-12-06 19:41:15 +0800

    Modify the comments in the USART header file

--- keil/Drivers/Driver_USART/stm32f10x_driver_usart.h
+++ keil/Drivers/Driver_USART/stm32f10x_driver_usart.h
@@ -1,15 +1,27 @@
 /*******************************************************************************
-Copyright (C), 2016-2016, Team MicroDynamics.
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
 
 Filename:    stm32f10x_driver_usart.h
 Author:      maksyuki
-Version:     1.0
-Date:        2016.8.15
+Version:     0.1.0.20161231_release
+Create date: 2016.08.15
 Description: define the serial port operation
 Others:      none
 Function List:
-             1. extern void usart_Init(u32 bound);
-History:     none
+             1. void usart_Init(u32 bound);
+             2. void USART_ClearBuf(UartBuf* RingBuf);
+             3. void USART_SendOneBytes(unsigned char dat);
+             4. uint8_t USART_SendOneBytesReturn(unsigned char dat);
+             5. uint8_t USART_ReadBuf(UartBuf* RingBuf);
+             6. uint16_t USART_CountBuf(UartBuf* RingBuf);
+             7. void USART_WriteBuf(UartBuf* RingBuf, uint8_t dat);
+             8. void USART_SendBuf(uint8_t* dat, uint8_t len);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2016.12.06  modify the module
 *******************************************************************************/
 
 #ifndef __STM32F10X_DRIVER_USART_H__
@@ -17,12 +29,12 @@ History:     none
 
 #include "stm32f10x.h"
 
-#define USART_REC_LEN 200               /*定义最大接收字节数*/
+#define USART_REC_LEN 200               /* The maximum number of bytes received */
 
-extern u8 USART_RX_BUF[USART_REC_LEN];  /*接收缓冲，最大USART_REC_LEN个字节，最后一个字节为换行符*/
-extern u16 USART_RX_STA;                /*接收状态标记*/
+extern u8 USART_RX_BUF[USART_REC_LEN];  /* Receive buffer, the last char is newline */
+extern u16 USART_RX_STA;                /* The flag of receiving status */
 
-/*USART receiver buffer*/
+/* USART receiver buffer */
 #define RX_BUFFER_SIZE   128
 #define TX_BUFFER_SIZE   128
 
@@ -37,10 +49,11 @@ typedef struct
     uint8_t* pbuf;
 }UartBuf;
 
-extern UartBuf UartTxbuf;  /*环形发送队列*/
-extern UartBuf UartRxbuf;  /*环形接收队列*/
+extern UartBuf UartTxbuf;  /* Ring send queue */
+extern UartBuf UartRxbuf;  /* Ring receive queue */
 
-extern void usart_Init(u32 bound);      /*USART_Init()在库函数中有*/
+/* USART_Init() exists in the official library */
+extern void usart_Init(u32 bound);      
 
 extern void USART_ClearBuf(UartBuf* RingBuf);
 extern void USART_SendOneBytes(unsigned char dat);

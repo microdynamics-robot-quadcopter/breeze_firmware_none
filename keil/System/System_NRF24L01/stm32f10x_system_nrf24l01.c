4d1b3419265bdad14d8b2b53cfd7a68e5d182845
maksyuki <myc20404401@sina.com>
2017-01-10 20:01:28 +0800

    Modify the comments in the NRF24L01 module

--- keil/System/System_NRF24L01/stm32f10x_system_nrf24l01.c
+++ keil/System/System_NRF24L01/stm32f10x_system_nrf24l01.c
@@ -1,3 +1,34 @@
+/*******************************************************************************
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
+
+Filename:    stm32f10x_system_nrf24l04.c
+Author:      maksyuki
+Version:     0.1.0.20161231_release
+Create date: 2016.08.14
+Description: implement the nrf24l01 function
+Others:      none
+Function List:
+             1. void NRF24L01_Init(void);
+             2. void SetRX_Mode(void);
+             3. void SetTX_Mode(void);
+             4. void NRF_TxPacket(uint8_t *tx_buf, uint8_t len);
+             5. uint8_t NRF_RxPacket(uint8_t *rx_buf, uint8_t len);
+             6. u8 NRF24L01_RxPacket(u8 *rxbuf);
+             7. uint8_t NRF_Write_Reg(uint8_t reg, uint8_t value);
+             8. uint8_t NRF_Write_Buf(uint8_t reg, uint8_t *pBuf, uint8_t len);
+             9. uint8_t NRF_Read_Reg(uint8_t reg);
+            10. uint8_t NRF_Read_Buf(uint8_t reg, uint8_t *pBuf, uint8_t len);
+            11. void NRF_Irq(void);
+            12. u8 NRF24L01_Check(void);
+            13. void NRF_Matching(void);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2017.01.10  modify the module
+*******************************************************************************/
+
 #include "stm32f10x_driver_spi.h"
 #include "stm32f10x_driver_delay.h"
 #include "stm32f10x_driver_eeprom.h"
@@ -7,147 +38,139 @@
 #include "stm32f10x_it.h"
 #include "stdio.h"
 
-uint8_t NRF24L01_RXDATA[RX_PLOAD_WIDTH];  /*NRF24l01接收到的数据*/
-uint8_t NRF24L01_TXDATA[RX_PLOAD_WIDTH];  /*NRF24l01需要发送的数据*/
+uint8_t NRF24L01_RXDATA[RX_PLOAD_WIDTH];  /* The received data of NRF24l01 */
+uint8_t NRF24L01_TXDATA[RX_PLOAD_WIDTH];  /* The data need to be transmit of NRF24l01 */
 
-/*修改该接收和发送地址，可以供多个飞行器在同一区域飞行，数据不受干扰*/
-u8 RX_ADDRESS[RX_ADR_WIDTH] = {0x34, 0xc3, 0x10, 0x10, 0x00};   /*接收地址*/
+/* Modify this address of receiving and transmitting, can support many equipments using in same region */
+u8 RX_ADDRESS[RX_ADR_WIDTH] = {0x34, 0xc3, 0x10, 0x10, 0x00};   /* Receiving address */
 
-/*初始化*/
 void NRF24L01_Init(void)
 {
     SPI_INIT();
 
-    /*check if NRF24L01 is in the SPI bus*/
+    /* Check if NRF24L01 is in the SPI bus */
     NRF24L01_Check();
 
-    /*set the NRF RX Address,priority to the latest address in eeprom*/
-    /*NRF24L01对频接收地址，优先匹配在EEPROM中上一次地址*/
+    /* Set the NRF RX address, priority to the latest address in eeprom */
     NRF_Matching();
 }
 
-/*接收模式*/
 void SetRX_Mode(void)
 {
     SPI_CE_L();
-    NRF_Write_Reg(FLUSH_RX, 0xff);                                                   /*清除TX FIFO寄存器*/
-    NRF_Write_Buf(NRF_WRITE_REG + RX_ADDR_P0, (uint8_t*) RX_ADDRESS, RX_ADR_WIDTH);  /*写RX节点地址*/
-    NRF_Write_Reg(NRF_WRITE_REG + EN_AA, 0x01);                                      /*使能通道0的自动应答*/
-    NRF_Write_Reg(NRF_WRITE_REG + EN_RXADDR, 0x01);                                  /*使能通道0的接收地址*/
-    NRF_Write_Reg(NRF_WRITE_REG + RF_CH, 40);                                        /*设置RF通信频率*/
-    NRF_Write_Reg(NRF_WRITE_REG + RX_PW_P0, RX_PLOAD_WIDTH);                         /*选择通道0的有效数据宽度*/
-    NRF_Write_Reg(NRF_WRITE_REG + RF_SETUP, 0x0f);                                   /*设置TX发射参数,0db增益,2Mbps,低噪声增益开启*/
-    NRF_Write_Reg(NRF_WRITE_REG + CONFIG, 0x0f);                                     /*配置基本工作模式的参数;PWR_UP,EN_CRC,16BIT_CRC,接收模式*/
+    NRF_Write_Reg(FLUSH_RX, 0xff);                                                   /* Clear TX FIFO register */
+    NRF_Write_Buf(NRF_WRITE_REG + RX_ADDR_P0, (uint8_t*) RX_ADDRESS, RX_ADR_WIDTH);  /* Write RX node address */
+    NRF_Write_Reg(NRF_WRITE_REG + EN_AA, 0x01);                                      /* Enable the automatic response of channel 0 */
+    NRF_Write_Reg(NRF_WRITE_REG + EN_RXADDR, 0x01);                                  /* Enable the receiving address of channel 0 */
+    NRF_Write_Reg(NRF_WRITE_REG + RF_CH, 40);                                        /* Set the communication frequency of RF */
+    NRF_Write_Reg(NRF_WRITE_REG + RX_PW_P0, RX_PLOAD_WIDTH);                         /* Select the valid data width of channel 0 */
+    NRF_Write_Reg(NRF_WRITE_REG + RF_SETUP, 0x0f);                                   /* Set parameters of TX: 0db, 2Mbps, enable low noise gain */
+    NRF_Write_Reg(NRF_WRITE_REG + CONFIG, 0x0f);                                     /* Set parameters: PWR_UP, EN_CRC, 16BIT_CRC, receive mode */
     SPI_CE_H();
     //printf("NRF24L01 Set to Receiving Mode,RX_ADDR 0x%x...\r\n",RX_ADDRESS[4]);
 }
 
-/*写数据包*/
 void NRF_TxPacket(uint8_t *tx_buf, uint8_t len)
 {
-    SPI_CE_L();                                 /*StandBy I模式*/
-    NRF_Write_Buf(WR_TX_PLOAD, tx_buf, len);    /*装载数据*/
-    SPI_CE_H();                                 /*置高CE，激发数据发送*/
+    SPI_CE_L();                               /* StandBy I mode */
+    NRF_Write_Buf(WR_TX_PLOAD, tx_buf, len);  /* Load data */
+    SPI_CE_H();                               /* Set high CE, enable data transmitting */
 }
 
-/*写寄存器*/
 uint8_t NRF_Write_Reg(uint8_t reg, uint8_t val)
 {
     uint8_t status;
     SPI_CSN_L();
     status = SPI_RW(reg);
-    SPI_RW(val);            /*写数据*/
-    SPI_CSN_H();            /*禁止该器件*/
+    SPI_RW(val);            /* Write data */
+    SPI_CSN_H();            /* Disable this component */
     return status;
 }
 
-/*读寄存器*/
 uint8_t NRF_Read_Reg(uint8_t reg)
 {
     uint8_t reg_val;
     SPI_CSN_L();
     SPI_RW(reg);
-    reg_val = SPI_RW(0);    /*读取该寄存器返回数据*/
+    reg_val = SPI_RW(0);    /* Read data */
     SPI_CSN_H();
     return reg_val;
 }
 
-/*读缓冲区*/
 uint8_t NRF_Read_Buf(uint8_t reg, uint8_t *pBuf, uint8_t len)
 {
     uint8_t i;
     uint8_t status;
-    SPI_CSN_L();                /*选通器件*/
-    status = SPI_RW(reg);       /*写寄存器地址*/
+    SPI_CSN_L();              /* Enable this component */
+    status = SPI_RW(reg);     /* Write register address */
 
     for (i = 0; i < len; i++)
     {
-        pBuf[i] = SPI_RW(0);    /*读取返回数据*/
+        pBuf[i] = SPI_RW(0);  /* Read data */
     }
 
-    SPI_CSN_H();                /*禁止该器件*/
+    SPI_CSN_H();              /* Disable this component */
     return status;
 }
 
-/*写缓冲区*/
 uint8_t NRF_Write_Buf(uint8_t reg, uint8_t *pBuf, uint8_t len)
 {
     uint8_t i;
     uint8_t status;
-    SPI_CSN_L();                /*选通器件*/
-    status = SPI_RW(reg);       /*写寄存器地址*/
+    SPI_CSN_L();           /* Enable this component */
+    status = SPI_RW(reg);  /* Write register address */
 
     for (i = 0; i < len; i++)
     {
-        SPI_RW(pBuf[i]);        /*写数据*/
+        SPI_RW(pBuf[i]);   /* Write data */
     }
 
-    SPI_CSN_H();                /*禁止该器件*/
+    SPI_CSN_H();           /* Disable this component */
     return status;
 }
 
-/*查询中断*/
+/* Query interrupt */
 void NRF_Irq(void)
 {
     uint8_t status = NRF_Read_Reg(NRF_READ_REG + NRFRegSTATUS);
-    if (status & (1 << RX_DR))                                       /*接收轮训标志位*/
+    if (status & (1 << RX_DR))                                       /* Receive the flag of polling */
     {
-        NRF_Read_Buf(RD_RX_PLOAD, NRF24L01_RXDATA, RX_PLOAD_WIDTH);  /*read receive payload from RX_FIFO buffer*/
-        ReceiveDataFromNRF();                                        /*自己做修改*/
-        NRF_Write_Reg(0x27, status);                                 /*清除nrf的中断标志位*/
+        NRF_Read_Buf(RD_RX_PLOAD, NRF24L01_RXDATA, RX_PLOAD_WIDTH);  /* Read receive payload from RX_FIFO buffer */
+        ReceiveDataFromNRF();                                        /* Can be modified */
+        NRF_Write_Reg(0x27, status);                                 /* Clear the interrupt flag of nrf */
         status = 0;
     }
 }
 
-/*接收函数*/
+/* Receive data */
 u8 NRF24L01_RxPacket(u8 *rxbuf)
 {
     u8 status;
 
-    //SPI2_SetSpeed(SPI_SPEED_4);                          /*spi速度为9Mhz(24L01的最大SPI时钟为10Mhz)*/
-	status = NRF_Read_Reg(NRFRegSTATUS);                   /*读取状态寄存器的值*/
-    NRF_Write_Reg(NRF_WRITE_REG + NRFRegSTATUS, status);   /*清除TX_DS或MAX_RT中断标志*/
+    //SPI2_SetSpeed(SPI_SPEED_4);                          /* The speed of spi is 9Mhz(maximum is 10Mhz) */
+    status = NRF_Read_Reg(NRFRegSTATUS);                   /* Read the value of status register */
+    NRF_Write_Reg(NRF_WRITE_REG + NRFRegSTATUS, status);   /* Clear the interrupt flag of TX_DS or MAX_RT */
 
-    if (status & RX_OK)                                    /*接收到数据*/
+    if (status & RX_OK)                                    /* Have received data */
     {
-        NRF_Read_Buf(RD_RX_PLOAD, rxbuf, RX_PLOAD_WIDTH);  /*读取数据*/
-        NRF_Write_Reg(FLUSH_RX, 0xff);                     /*清除RX FIFO寄存器*/
+        NRF_Read_Buf(RD_RX_PLOAD, rxbuf, RX_PLOAD_WIDTH);  /* Read data */
+        NRF_Write_Reg(FLUSH_RX, 0xff);                     /* Clear RX FIFO register */
         return 0;
     }
-    return 1;                                              /*没收到任何数据*/
+    return 1;                                              /* Never receive any data */
 }
 
-/*判断SPI接口接入的NRF芯片是否可用*/
+/* Determine whether the SPI interface access to the NRF chip is available */
 u8 NRF24L01_Check(void)
 {
     u8 i;
     u8 buf1[5] = {0xC2, 0xC2, 0xC2, 0xC2, 0xC2};
     u8 buf2[5];
 
-    NRF_Write_Buf(NRF_WRITE_REG + TX_ADDR, buf1, 5);                 /*写入5个字节的地址*/
-    NRF_Read_Buf(TX_ADDR, buf2, 5);                                  /*读出写入的地址*/
+    NRF_Write_Buf(NRF_WRITE_REG + TX_ADDR, buf1, 5);       /* Write 5 bytes address */
+    NRF_Read_Buf(TX_ADDR, buf2, 5);                        /* Read address that have been written */
 
-    /*比较*/
+    /* Compare */
     for (i = 0; i < 5; i++)
     {
         if (buf2[i] != 0xC2)
@@ -156,8 +179,8 @@ u8 NRF24L01_Check(void)
         }
     }
 
-    if (i == 5) {printf("NRF24L01 found...\r\n"); return 1;}         /*MCU与NRF成功连接*/ 
-    else        {printf("NRF24L01 check failed...\r\n"); return 0;}  /*MCU与NRF不正常连接*/
+    if (i == 5) {printf("NRF24L01 found...\r\n"); return 1;}         /* MCU connects to NRF successfully */
+    else        {printf("NRF24L01 check failed...\r\n"); return 0;}  /* MCU connects to NRF unsuccessfully */
 }
 
 static uint8_t sta;
@@ -167,7 +190,7 @@ u8 NRFMatched = 0;
 void NRF_Matching(void)
 {
     static uint32_t nTs, nT;
-    static uint32_t writeOvertime = 2 * 1000000;  /*unit:us*/
+    static uint32_t writeOvertime = 2 * 1000000;  /* Unit: us */
 
     LedA_On;
     LedB_On;
@@ -183,11 +206,11 @@ void NRF_Matching(void)
         if (nT >= writeOvertime)
         {
             RX_ADDRESS[4] = table.NRFAddr[4];
-            break;  /*exit when time out,and do not change original address*/
+            break;    /* Exit when time out, and do not change original address */
         }
 
-        SetRX_Mode();                                     /*reset RX mode write RX panel address*/
-        delay_ms(4);                                      /*delay is needed after reset NRF*/
+        SetRX_Mode(); /* Reset RX mode write RX panel address */
+        delay_ms(4);  /* Delay is needed after reset NRF */
         sta = NRF_Read_Reg(NRF_READ_REG + NRFRegSTATUS);
 
         if ((sta & 0x0E) == 0x00)
@@ -196,7 +219,7 @@ void NRF_Matching(void)
         }
         else
         {
-            RX_ADDRESS[4]++;            /*search the next RX_ADDRESS*/
+            RX_ADDRESS[4]++;   /* Search the next RX_ADDRESS */
             if (RX_ADDRESS[4] == 0xFF)
             {
                 RX_ADDRESS[4] = 0x00;
@@ -205,15 +228,15 @@ void NRF_Matching(void)
     }
     while ((sta & 0x0E) == 0x0E);
 
-    SetRX_Mode();                       /*reset RX mode*/
+    SetRX_Mode();              /* Reset RX mode */
 
     if ((NRFMatched == 1) && (RX_ADDRESS[4] != table.NRFAddr[4]))
     {
-        SaveParamsToEEPROM();           /*write eeprom when current addr != original addr*/
+        SaveParamsToEEPROM();  /* Write eeprom when current addr != original addr */
     }
 
     LedA_Off;
     LedB_Off;
-    LedC_Off;                           /*matching end*/
+    LedC_Off;                  /* Matching end */
     LedD_Off;
 }
--- keil/System/System_NRF24L01/stm32f10x_system_nrf24l01.h
+++ keil/System/System_NRF24L01/stm32f10x_system_nrf24l01.h
@@ -1,56 +1,87 @@
+/*******************************************************************************
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
+
+Filename:    stm32f10x_system_nrf24l04.h
+Author:      maksyuki
+Version:     0.1.0.20161231_release
+Create date: 2016.08.14
+Description: declare the nrf24l01 function
+Others:      none
+Function List:
+             1. void NRF24L01_Init(void);
+             2. void SetRX_Mode(void);
+             3. void SetTX_Mode(void);
+             4. void NRF_TxPacket(uint8_t *tx_buf, uint8_t len);
+             5. uint8_t NRF_RxPacket(uint8_t *rx_buf, uint8_t len);
+             6. u8 NRF24L01_RxPacket(u8 *rxbuf);
+             7. uint8_t NRF_Write_Reg(uint8_t reg, uint8_t value);
+             8. uint8_t NRF_Write_Buf(uint8_t reg, uint8_t *pBuf, uint8_t len);
+             9. uint8_t NRF_Read_Reg(uint8_t reg);
+            10. uint8_t NRF_Read_Buf(uint8_t reg, uint8_t *pBuf, uint8_t len);
+            11. void NRF_Irq(void);
+            12. u8 NRF24L01_Check(void);
+            13. void NRF_Matching(void);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2017.01.10  modify the module
+*******************************************************************************/
+
 #ifndef __STM32F10X_SYSTEM_NRF24L01_H__
 #define __STM32F10X_SYSTEM_NRF24L01_H__
 
 #include "stm32f10x.h"
 
-//*********************************************NRF24L01*************************************
-#define TX_ADR_WIDTH    5       /*5 uints TX address width*/
-#define RX_ADR_WIDTH    5       /*5 uints RX address width*/
+//***************************************NRF24L01*******************************
+#define TX_ADR_WIDTH    5       /* 5 uints TX address width */
+#define RX_ADR_WIDTH    5       /* 5 uints RX address width */
 
-#define RX_PLOAD_WIDTH  32      /*32 uints TX payload*/
-#define TX_PLOAD_WIDTH  32      /*32 uints TX payload*/
-//***************************************NRF24L01寄存器指令*******************************************************
-#define NRF_READ_REG    0x00    /*读寄存器指令*/
-#define NRF_WRITE_REG   0x20    /*写寄存器指令*/
-#define RD_RX_PLOAD     0x61    /*读取接收数据指令*/
-#define WR_TX_PLOAD     0xA0    /*写待发数据指令*/
-#define FLUSH_TX        0xE1    /*冲洗发送FIFO指令*/
-#define FLUSH_RX        0xE2    /*冲洗接收FIFO指令*/
-#define REUSE_TX_PL     0xE3    /*定义重复装载数据指令*/
-#define NOP             0xFF    /*保留*/
-//*************************************SPI(nRF24L01)寄存器地址****************************************************
-#define CONFIG          0x00    /*配置收发状态，CRC校验模式以及收发状态响应方式*/
-#define EN_AA           0x01    /*自动应答功能设置*/
-#define EN_RXADDR       0x02    /*可用信道设置*/
-#define SETUP_AW        0x03    /*收发地址宽度设置*/
-#define SETUP_RETR      0x04    /*自动重发功能设置*/
-#define RF_CH           0x05    /*工作频率设置*/
-#define RF_SETUP        0x06    /*发射速率、功耗功能设置*/
-#define NRFRegSTATUS    0x07    /*状态寄存器*/
-#define OBSERVE_TX      0x08    /*发送监测功能*/
-#define CD              0x09    /*地址检测*/
-#define RX_ADDR_P0      0x0A    /*频道0接收数据地址*/
-#define RX_ADDR_P1      0x0B    /*频道1接收数据地址*/
-#define RX_ADDR_P2      0x0C    /*频道2接收数据地址*/
-#define RX_ADDR_P3      0x0D    /*频道3接收数据地址*/
-#define RX_ADDR_P4      0x0E    /*频道4接收数据地址*/
-#define RX_ADDR_P5      0x0F    /*频道5接收数据地址*/
-#define TX_ADDR         0x10    /*发送地址寄存器*/
-#define RX_PW_P0        0x11    /*接收频道0接收数据长度*/
-#define RX_PW_P1        0x12    /*接收频道1接收数据长度*/
-#define RX_PW_P2        0x13    /*接收频道2接收数据长度*/
-#define RX_PW_P3        0x14    /*接收频道3接收数据长度*/
-#define RX_PW_P4        0x15    /*接收频道4接收数据长度*/
-#define RX_PW_P5        0x16    /*接收频道5接收数据长度*/
-#define FIFO_STATUS     0x17    /*FIFO栈入栈出状态寄存器设置*/
-//**************************************************************************************
-#define RX_DR           6       /*中断标志*/
+#define RX_PLOAD_WIDTH  32      /* 32 uints TX payload */
+#define TX_PLOAD_WIDTH  32      /* 32 uints TX payload */
+//***************************************NRF24L01 register instructions*********
+#define NRF_READ_REG    0x00    /* Read register */
+#define NRF_WRITE_REG   0x20    /* Write register */
+#define RD_RX_PLOAD     0x61    /* Read receiving data */
+#define WR_TX_PLOAD     0xA0    /* Write transmitting data */
+#define FLUSH_TX        0xE1    /* Flash transmitting FIFO */
+#define FLUSH_RX        0xE2    /* Flash receiving FIFO */
+#define REUSE_TX_PL     0xE3    /* Reload data */
+#define NOP             0xFF    /* Reserve */
+//*************************************SPI(nRF24L01) registers address**********
+#define CONFIG          0x00    /* Configure */
+#define EN_AA           0x01    /* Set the automatic response */
+#define EN_RXADDR       0x02    /* Set the available channel */
+#define SETUP_AW        0x03    /* Set the width of receiving and transmitting's address */
+#define SETUP_RETR      0x04    /* Set the automatic resend */
+#define RF_CH           0x05    /* Set the working frequency */
+#define RF_SETUP        0x06    /* Set the transmitting rate, power */
+#define NRFRegSTATUS    0x07    /* Status register */
+#define OBSERVE_TX      0x08    /* Transmission monitoring */
+#define CD              0x09    /* Address detection */
+#define RX_ADDR_P0      0x0A    /* The receiving address of channel 0 */
+#define RX_ADDR_P1      0x0B    /* The receiving address of channel 1 */
+#define RX_ADDR_P2      0x0C    /* The receiving address of channel 2 */
+#define RX_ADDR_P3      0x0D    /* The receiving address of channel 3 */
+#define RX_ADDR_P4      0x0E    /* The receiving address of channel 4 */
+#define RX_ADDR_P5      0x0F    /* The receiving address of channel 5 */
+#define TX_ADDR         0x10    /* Transmitting address register */
+#define RX_PW_P0        0x11    /* The receiving data's length of channel 0 */
+#define RX_PW_P1        0x12    /* The receiving data's length of channel 1 */
+#define RX_PW_P2        0x13    /* The receiving data's length of channel 2 */
+#define RX_PW_P3        0x14    /* The receiving data's length of channel 3 */
+#define RX_PW_P4        0x15    /* The receiving data's length of channel 4 */
+#define RX_PW_P5        0x16    /* The receiving data's length of channel 5 */
+#define FIFO_STATUS     0x17    /* Set the status register of FIFO */
+//*****************************************************************************
+#define RX_DR           6       /* The flag of interrupt */
 #define TX_DS           5
 #define MAX_RT          4
 
-#define MAX_TX          0x10   /*达到最大发送次数中断*/
-#define TX_OK           0x20   /*TX发送完成中断*/
-#define RX_OK           0x40   /*接收到数据中断*/
+#define MAX_TX          0x10   /* Maximum number of transmissions interrupt */
+#define TX_OK           0x20   /* Transmitting data interrupt */
+#define RX_OK           0x40   /* Receiving data interrupt */
 
 extern void NRF24L01_Init(void);
 extern void SetRX_Mode(void);

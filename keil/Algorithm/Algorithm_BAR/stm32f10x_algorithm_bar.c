7f03541e8094510c562dcbcbdf0f0c412a38026b
maksyuki <myc20404401@sina.com>
2017-01-11 11:02:15 +0800

    Modify the comments in the BAR module

--- keil/Algorithm/Algorithm_BAR/stm32f10x_algorithm_bar.c
+++ keil/Algorithm/Algorithm_BAR/stm32f10x_algorithm_bar.c
@@ -1,10 +1,32 @@
+/*******************************************************************************
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
+
+Filename:    stm32f10x_algorithm_bar.c
+Author:      maksyuki
+Version:     0.1.0.20161231_release
+Create date: 2016.09.14
+Description: implement the barometer function
+Others:      none
+Function List:
+             1. static void Intertial_Filter_Predict(float dt, float x[3]);
+             2. static void Intertial_Filter_Corrent(float e, float dt,
+                                                     float x[3], int i, float w);
+             3. void AltitudeCombineThread(void);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2017.01.11  modify the module
+*******************************************************************************/
+
 #include "stm32f10x_system_ms5611.h"
 #include "stm32f10x_algorithm_bar.h"
 #include "stm32f10x_algorithm_imu.h"
 #include "stm32f10x_it.h"
 
-nav_t nav;       /*NED frame in earth*/
-float z_est[3];  /*estimate z vz az*/
+nav_t nav;       /* NED frame in earth */
+float z_est[3];  /* Estimate z vz az */
 
 uint8_t accUpdated = 0;
 
@@ -12,16 +34,16 @@ static float w_z_bar    = 0.5f;
 static float w_z_acc    = 20.0f;
 static float w_acc_bias = 0.05f;
 
-/*acceleration in NED frame*/
+/* Acceleration in NED frame */
 float accel_NED[3] = {0.0f, 0.0f, -CONSTANTS_ONE_G};
 
-float corr_bar = 0.0f;  /*m*/
+float corr_bar = 0.0f;  /* m */
 
-/*store error when sensor updates, but correct on each time step to avoid jumps in estimated value*/
-float corr_acc[3] = {0.0f, 0.0f, 0.0f};  /*NED m/s2*/
-float acc_bias[3] = {0.0f, 0.0f, 0.0f};  /*body frame*/
+/* Store error when sensor updates, but correct on each time step to avoid jumps in estimated value */
+float corr_acc[3] = {0.0f, 0.0f, 0.0f};  /* NED m/s2 */
+float acc_bias[3] = {0.0f, 0.0f, 0.0f};  /* Body frame */
 
-/*combine filter to correct err*/
+/* Combine filter to correct err */
 static void Interial_Filter_Predict(float dt, float x[3])
 {
     x[0] += x[1] * dt + x[2] * dt * dt / 2.0f;
@@ -44,17 +66,17 @@ static void Interial_Filter_Correct(float e, float dt, float x[3], int i, float 
     }
 }
 
-/*timeStamp in us. Thread should be executed every 2~20ms*/
-/*MS5611_Altitude, should be in m. (can be fixed to abs, not relative). positive above ground*/
-/*accFilted, should be filted*/
-/*为了能够提高气压计的精度，这里融合了加速度计的数据*/
+/* TimeStamp in us. Thread should be executed every 2~20ms */
+/* MS5611_Altitude, should be in m. (can be fixed to abs, not relative). positive above ground */
+/* accFilted, should be filted */
+/* To improve the accuracy of the barometer, fuse the data of accelerometer */
 void AltitudeCombineThread(void)
 {
     static uint32_t PreTime = 0;
     uint32_t NowTime;
     float dt;
 
-    /*accelerometer bias correction*/
+    /* Accelerometer bias correction */
     float accel_bias_corr[3] = {0.0f, 0.0f, 0.0f};
     uint8_t i, j;
 
@@ -62,7 +84,7 @@ void AltitudeCombineThread(void)
     dt = (PreTime > 0) ? ((NowTime - PreTime) / 1000000.0f) : 0;
     PreTime = NowTime;
     
-    if (!PaOffsetInited)   /*wait baro to init its offset*/
+    if (!PaOffsetInited)   /* Wait baro to init its offset */
     {
         return;
     }
@@ -72,9 +94,9 @@ void AltitudeCombineThread(void)
         return;
     }
     
-    if (Baro_Alt_Updated)  /*store err when sensor update 后面应该在sensor数值后加一个timeStamp，判断是否更新*/
+    if (Baro_Alt_Updated)  /* Store err when sensor update */
     {
-        corr_bar = 0 - MS5611_Altitude - z_est[0];  /*MS5611_Altitude baro alt, is postive above offset level. not in NED. z_est is in NED frame.*/
+        corr_bar = 0 - MS5611_Altitude - z_est[0];  /* MS5611_Altitude baro alt, is postive above offset level. not in NED. z_est is in NED frame. */
         Baro_Alt_Updated = 0;
     }
     
@@ -98,10 +120,10 @@ void AltitudeCombineThread(void)
         accUpdated   = 0;
     }
 
-    /*correct accelerometer bias every time step*/
+    /* Correct accelerometer bias every time step */
     accel_bias_corr[2] -= corr_bar * w_z_bar * w_z_bar;
 
-    /*transform error vector from NED frame to body frame*/
+    /* Transform error vector from NED frame to body frame */
     for (i = 0; i < 3; i++)
     {
         float c = 0.0f;
@@ -114,7 +136,7 @@ void AltitudeCombineThread(void)
 
     acc_bias[2] = -acc_bias[2];
 
-    /*inertial filter prediction for altitude ???*/
+    /* Inertial filter prediction for altitude ??? */
     Interial_Filter_Predict(dt, z_est);
     Interial_Filter_Correct(corr_bar, dt, z_est, 0, w_z_bar);
     Interial_Filter_Correct(corr_acc[2], dt, z_est, 2, w_z_acc);
--- keil/Algorithm/Algorithm_BAR/stm32f10x_algorithm_bar.h
+++ keil/Algorithm/Algorithm_BAR/stm32f10x_algorithm_bar.h
@@ -1,9 +1,31 @@
+/*******************************************************************************
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT 
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
+
+Filename:    stm32f10x_algorithm_bar.h
+Author:      maksyuki
+Version:     0.1.0.20161231_release
+Create date: 2016.09.14
+Description: declare the barometer function
+Others:      none
+Function List:
+             1. static void Intertial_Filter_Predict(float dt, float x[3]);
+             2. static void Intertial_Filter_Corrent(float e, float dt,
+                                                     float x[3], int i, float w);
+             3. void AltitudeCombineThread(void);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2017.01.11  modify the module
+*******************************************************************************/
+
 #ifndef __STM32F10X_ALGORITHM_BAR_H__
 #define __STM32F10X_ALGORITHM_BAR_H__
 
 #include "stm32f10x.h"
 
-/*us store error when sensor updates, but correct on each time step to avoid jumps in estimated value */
+/* us store error when sensor updates, but correct on each time step to avoid jumps in estimated value */
 #define ALT_THREAD_RPD 5000
 
 typedef struct NAV_TT
@@ -20,13 +42,12 @@ typedef struct NAV_TT
 }nav_t;
 
 extern nav_t nav;
-extern float z_est[3];  /*estimate z vz az*/
+extern float z_est[3];  /* Estimate z vz az */
 extern uint8_t landed;
 extern uint8_t accUpdated;
 
 static void Intertial_Filter_Predict(float dt, float x[3]);
 static void Intertial_Filter_Corrent(float e, float dt, float x[3], int i, float w);
-
 extern void AltitudeCombineThread(void);
 
 #endif

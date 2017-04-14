a1b8e4f2199d082013b4263dd47fe35853888240
myyerrol <myyerrol@126.com>
2017-04-14 14:23:49 +0800

    Add a option

--- make/Tools/setup.sh
+++ make/Tools/setup.sh
@@ -3,7 +3,7 @@
 # If abort error, exit shell script.
 set -e
 
-# Set the gcc-arm tool chains
+# Install the gcc-arm tool chains
 if command -v arm-none-eabi-gcc > /dev/null 2>&1; then
     echo "The gcc-arm tool chains has been installed!"
 else
@@ -15,7 +15,7 @@ else
     echo "Installing the gcc-arm tool chains successfully!"
 fi
 
-# Set the openocd
+# Install the openocd
 if command -v openocd > /dev/null 2>&1; then
     echo "The openocd has been installed!"
 else
@@ -23,4 +23,14 @@ else
     echo "Installing the openocd..."
     sudo apt-get install openocd
     echo "Installing the openocd successfully!"
+fi
+
+# Install the minicom
+if command -v minicom > /dev/null 2>&1; then
+    echo "The minicom has been installed!"
+else
+    echo "The minicom has not been installed!"
+    echo "Installing the minicom..."
+    sudo apt-get install minicom
+    echo "Installing the minicom successfully!"
 fi

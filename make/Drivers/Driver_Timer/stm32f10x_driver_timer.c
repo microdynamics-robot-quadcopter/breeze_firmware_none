521af922c464df0640660268cd8c0132e614a7f6
myyerrol <myyerrol@126.com>
2017-04-24 19:31:32 +0800

    Add the driver of Timer

---
+++ make/Drivers/Driver_Timer/stm32f10x_driver_timer.c
@@ -0,0 +1,73 @@
+/*******************************************************************************
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
+
+Filename:    stm32f10x_driver_timer.c
+Author:      maksyuki
+Version:     0.1.0.20161231_release
+Create date: 2016.08.17
+Description: implement the timer set operation
+Others:      none
+Function List:
+             1. void TIM4_Init(u16 arr, u16 psc);
+History:
+1. <author>    <date>         <desc>
+   maksyuki  2016.12.03  modify the module
+*******************************************************************************/
+
+#include "stm32f10x_driver_nvic.h"
+#include "stm32f10x_driver_timer.h"
+
+vu16 loop_cnt_10hz  = 0;
+vu16 loop_cnt_20hz  = 0;
+vu16 loop_cnt_50hz  = 0;
+vu16 loop_cnt_100hz = 0;
+
+bool loop_flag_10hz  = false;
+bool loop_flag_20hz  = false;
+bool loop_flag_50hz  = false;
+bool loop_flag_100hz = false;
+
+void Timer_InitTIM3(u16 arr, u16 psc)
+{
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+    NVIC_InitTIM3();
+
+    TIM_DeInit(TIM3);
+
+    TIM_TimeBaseStructure.TIM_Period        = arr;
+    TIM_TimeBaseStructure.TIM_Prescaler     = psc - 1;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode   = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+    TIM_ClearFlag(TIM3, TIM_FLAG_Update);
+    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM3, ENABLE);
+}
+
+void Timer_InitTIM4(u16 arr, u16 psc)
+{
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+    NVIC_InitTIM4();
+
+    TIM_DeInit(TIM4);
+
+    TIM_TimeBaseStructure.TIM_Period        = arr;
+    TIM_TimeBaseStructure.TIM_Prescaler     = psc - 1;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode   = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+
+    TIM_ClearFlag(TIM4, TIM_FLAG_Update);
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM4, ENABLE);
+}
---
+++ make/Drivers/Driver_Timer/stm32f10x_driver_timer.h
@@ -0,0 +1,41 @@
+/*******************************************************************************
+THIS PROGRAM IS FREE SOFTWARE. YOU CAN REDISTRIBUTE IT AND/OR MODIFY IT
+UNDER THE TERMS OF THE GNU GPLV3 AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION.
+
+Copyright (C), 2016-2016, Team MicroDynamics <microdynamics@126.com>
+
+Filename:    stm32f10x_driver_timer.h
+Author:      maksyuki
+Version:     0.1.0.20161231_release
+Create date: 2016.08.17
+Description: Declare the timer set operation
+Others:      none
+Function List:
+History:
+<author>    <date>        <desc>
+maksyuki    2016.12.03    Modify the module
+myyerrol    2017.04.24    Format the module
+*******************************************************************************/
+
+#ifndef __STM32F10X_DRIVER_TIM_H__
+#define __STM32F10X_DRIVER_TIM_H__
+
+#include <stdbool.h>
+#include "stm32f10x.h"
+
+// The interrupt variables of timer for main control.
+extern vu16 loop_cnt_10hz;
+extern vu16 loop_cnt_20hz;
+extern vu16 loop_cnt_50hz;
+extern vu16 loop_cnt_100hz;
+
+// The interrupt flags of timer for main control.
+extern bool loop_flag_10hz;
+extern bool loop_flag_20hz;
+extern bool loop_flag_50hz;
+extern bool loop_flag_100hz;
+
+extern void Timer_InitTIM3(u16 arr, u16 psc);
+extern void Timer_InitTIM4(u16 arr, u16 psc);
+
+#endif
